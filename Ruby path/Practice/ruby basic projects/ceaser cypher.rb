8ce620aa3a26cabbb3d76de6bbd9808ee8684877
meamalpb <321amalpb.com@gmail.com>
2023-03-22 14:09:25 +0530

    ceaser cypher practice problem done(1)

--- Ruby path/Practice/ruby basic projects/ceaser cypher.rb
+++ Ruby path/Practice/ruby basic projects/ceaser cypher.rb
@@ -3,8 +3,8 @@ def ceacypher(word,number)
     number = number.to_i
     res=""
     word.each_char{|l| 
-        #upcase
         
+    #upcase
     if(l.ord.between?(65,90))
             rl = l.ord + number
             if(rl.between?(65,90))
